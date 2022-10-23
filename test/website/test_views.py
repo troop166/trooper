5fb4f8cd51ff88bc39131126d82624ff654cebf1
John Lawrence <thirdjal@users.noreply.github.com>
2022-10-22 22:42:44 -0700

    Favicon is dynamically generated now. Remove failing test to static

--- test/website/test_views.py
+++ test/website/test_views.py
@@ -1,17 +1,7 @@
 from http import HTTPStatus
 
-from django.test import SimpleTestCase, TestCase
+from django.test import TestCase
 from django.urls import reverse
-
-
-class FaviconTests(SimpleTestCase):
-    def test_get(self):
-        response = self.client.get("/favicon.ico")
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(response["Cache-Control"], "max-age=86400, immutable, public")
-        self.assertEqual(response["Content-Type"], "image/x-icon")
-        self.assertGreater(len(response.getvalue()), 0)
 
 
 class AboutPageTests(TestCase):
