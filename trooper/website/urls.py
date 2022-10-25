3f731e2739daff61808709a5091307c1b3961714
John Lawrence <thirdjal@noreply.users.github.com>
2022-10-24 18:05:28 -0700

    Move Home/About pages into website app

--- trooper/website/urls.py
+++ trooper/website/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from trooper.website import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="website/index.html"), name="home"),
-    path("favicon.ico", views.favicon),
-    path(
-        "about/", TemplateView.as_view(template_name="website/about.html"), name="about"
-    ),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
 ]
--- trooper/website/views.py
+++ trooper/website/views.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles import finders
 from django.http import FileResponse
 from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
+from django.views.generic import TemplateView
 
 
 @require_GET
@@ -9,3 +10,11 @@ from django.views.decorators.http import require_GET
 def favicon(request):
     file = finders.find("img/favicon.ico")
     return FileResponse(open(file, "rb"))
+
+
+class HomePageView(TemplateView):
+    template_name = "website/index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "website/about.html"
