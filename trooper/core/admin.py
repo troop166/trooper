c8447690090c8780adff33045028345f797c980e
John Lawrence <thirdjal@noreply.users.github.com>
2022-10-25 13:46:54 -0700

    Bugfix: Allow migrations to continue on Postgres if website_configuration doesn't yet exist

--- trooper/core/admin.py
+++ trooper/core/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.db import OperationalError
+from django.db import OperationalError, ProgrammingError
 from django.utils.translation import gettext as _
 
 from trooper.website.models import Configuration
 
 try:
     name = Configuration.current().name
-except (AttributeError, OperationalError):
+except (AttributeError, OperationalError, ProgrammingError):
     name = Configuration.name
 
 admin.site.login = login_required(admin.site.login)
