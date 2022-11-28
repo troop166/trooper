254def65f4d356d435834cf03420b60ef18a2ea1
John Lawrence <thirdjal@noreply.users.github.com>
2022-11-28 09:50:38 -0800

    Explain what the RegEx is supposed to do

--- trooper/calendars/converters.py
+++ trooper/calendars/converters.py
@@ -2,8 +2,10 @@ from django.urls.converters import IntConverter
 
 
 class MonthConverter(IntConverter):
+    # Matches MM from 1-12 with optional leading zero
     regex = "(1[0-2]|0?[1-9])"
 
 
 class YearConverter(IntConverter):
+    # Matches YYYY from 1900-2999
     regex = "(19|2[0-9])[0-9]{2}"
