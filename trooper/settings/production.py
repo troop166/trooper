f30faafec5a29635bd61fa5062c15a21c6c28fc3
John Lawrence <thirdjal@noreply.users.github.com>
2023-01-12 15:52:31 -0800

    Support for Sentry for remote monitoring

--- trooper/settings/production.py
+++ trooper/settings/production.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -17,3 +20,12 @@ SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=60 * 60 * 24)
 SECURE_PROXY_SSL_HEADER = env.tuple("SECURE_PROXY_SSL_HEADER", default=None)
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=True)
 SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", default=True)
+
+# Sentry Support
+# https://docs.sentry.io/platforms/python/guides/django/
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN", default=""),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=env.int("SENTRY_TRACES_SAMPLE_RATE", default=1.0),
+    send_default_pii=env.bool("SENTRY_SEND_DEFAULT_PII", default=True),
+)
