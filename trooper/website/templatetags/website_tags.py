8f2ac089685621fc5c7bbc95af7b74c4786a47a2
John Lawrence <thirdjal@noreply.users.github.com>
2023-01-27 07:45:39 -0800

    Simplify link tags in navbar

--- trooper/website/templatetags/website_tags.py
+++ trooper/website/templatetags/website_tags.py
@@ -8,7 +8,16 @@ register = template.Library()
 @register.inclusion_tag("website/partials/navbar.html", takes_context=True)
 def navbar(context):
     user = context["user"]
-    links = Page.objects.in_navbar_for(user)
+    pages = Page.objects.in_navbar_for(user)
+    context["navbar"] = {"links": []}
 
-    context["navbar"] = {"links": links}
+    for page in pages:
+        context["navbar"]["links"].append(
+            {
+                "title": page.title,
+                "url": page.get_absolute_url(),
+                "is_current": context["request"].path == page.get_absolute_url(),
+            }
+        )
+
     return context
