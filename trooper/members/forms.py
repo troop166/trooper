c169f6ec56dbf78ba135c5cf645f04fb2ffc5563
John Lawrence <thirdjal@noreply.users.github.com>
2023-02-15 16:22:02 -0800

    Make password optional in MemberCreationForm

--- trooper/members/forms.py
+++ trooper/members/forms.py
@@ -13,6 +13,21 @@ from trooper.members.models import Member
 
 
 class MemberCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        required=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        required=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
     class Meta(UserCreationForm.Meta):
         model = Member
         fields = ("first_name", "last_name", "date_of_birth", "gender")
