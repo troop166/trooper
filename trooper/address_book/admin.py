0b9e31a8414ff3b5022e38f70542d6e0050dbd52
John Lawrence <thirdjal@noreply.users.github.com>
2023-04-06 19:07:09 -0700

    Simplify AddressBook models

--- trooper/address_book/admin.py
+++ trooper/address_book/admin.py
@@ -1,36 +1,31 @@
-from django.contrib.contenttypes.admin import GenericStackedInline, GenericTabularInline
+from django.contrib import admin
 
-from trooper.address_book.forms import AddressForm
+from trooper.address_book.forms import AddressForm, EmailForm, PhoneForm
 from trooper.address_book.models import Address, Email, Phone
 
 
-class AddressInline(GenericStackedInline):
-    model = Address
-    extra = 0
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    form = AddressForm
     fieldsets = (
         (
             None,
             {
                 "fields": (
-                    "label",
                     "street",
                     "street2",
                     ("city", "state", "zip_code"),
-                    "is_published",
                 )
             },
         ),
     )
-    form = AddressForm
 
 
-class EmailInline(GenericTabularInline):
-    model = Email
-    extra = 0
-    fields = ("label", "address", "is_published", "is_subscribed")
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    form = EmailForm
 
 
-class PhoneInline(GenericTabularInline):
-    model = Phone
-    extra = 0
-    fields = ("label", "number", "is_published")
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    form = PhoneForm
--- trooper/address_book/models.py
+++ trooper/address_book/models.py
@@ -1,5 +1,3 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.functional import cached_property
 from django.utils.html import format_html
@@ -9,24 +7,7 @@ from localflavor.us.models import USStateField, USZipCodeField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class AddressBookQuerySet(models.QuerySet):
-    def published(self):
-        return self.filter(is_published=True)
-
-    def subscribed(self):
-        return self.filter(is_subscribed=True)
-
-
 class Address(models.Model):
-    class Label(models.TextChoices):
-        HOME = "HOME", _("Home")
-        WORK = "WORK", _("Work")
-        SCHOOL = "SCHOOL", _("School")
-        PO_BOX = "POB", _("P.O. Box")
-
-    label = models.CharField(
-        _("label"), max_length=6, choices=Label.choices, blank=True
-    )
     street = models.CharField(_("street"), max_length=150)
     street2 = models.CharField(
         _("street 2"),
@@ -37,24 +18,8 @@ class Address(models.Model):
     city = models.CharField(_("city"), max_length=50)
     state = USStateField(_("state"))
     zip_code = USZipCodeField(_("ZIP code"))
-    is_published = models.BooleanField(
-        _("published in the directory"),
-        default=True,
-        help_text=_("Allow other members to see this address in the Troop directory."),
-    )
-
-    # Mandatory fields for generic relation
-    # https://docs.djangoproject.com/en/stable/ref/contrib/contenttypes/#generic-relations
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.UUIDField()
-    content_object = GenericForeignKey()
-
-    objects = AddressBookQuerySet.as_manager()
 
     class Meta:
-        indexes = [
-            models.Index(fields=["content_type", "object_id"]),
-        ]
         verbose_name = _("Address")
         verbose_name_plural = _("Addresses")
 
@@ -84,38 +49,9 @@ class Address(models.Model):
 
 
 class Email(models.Model):
-    class Label(models.TextChoices):
-        HOME = "HOME", _("Home")
-        SCHOOL = "SCHOOL", _("School")
-        WORK = "WORK", _("Work")
-
-    label = models.CharField(
-        _("label"), max_length=6, choices=Label.choices, blank=True
-    )
     address = models.EmailField(_("email address"), unique=True)
-    is_published = models.BooleanField(
-        _("published in directory"),
-        default=True,
-        help_text=_("Allow others to see this address in the member directory."),
-    )
-    is_subscribed = models.BooleanField(
-        _("subscribed to mailing lists"),
-        default=True,
-        help_text=_("Receive periodic email communications at this address."),
-    )
-
-    # Mandatory fields for generic relation
-    # https://docs.djangoproject.com/en/stable/ref/contrib/contenttypes/#generic-relations
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.UUIDField()
-    content_object = GenericForeignKey()
-
-    objects = AddressBookQuerySet.as_manager()
 
     class Meta:
-        indexes = [
-            models.Index(fields=["content_type", "object_id"]),
-        ]
         verbose_name = _("Email Address")
         verbose_name_plural = _("Email Addresses")
 
@@ -124,38 +60,24 @@ class Email(models.Model):
 
 
 class Phone(models.Model):
-    class Label(models.TextChoices):
-        """A subset of phone types from RFC 2426."""
-
-        HOME = "HOME", _("Home")
-        MOBILE = "CELL", _("Mobile")
-        FAX = "FAX", _("Fax")
-        WORK = "WORK", _("Work")
-
-    label = models.CharField(
-        _("label"), max_length=4, choices=Label.choices, blank=True
-    )
     number = PhoneNumberField(_("phone number"))
-    is_published = models.BooleanField(
-        _("published in directory"),
-        default=True,
-        help_text=_("Allow other members to see this number in the Troop directory."),
-    )
-
-    # Mandatory fields for generic relation
-    # https://docs.djangoproject.com/en/stable/ref/contrib/contenttypes/#generic-relations
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.UUIDField()
-    content_object = GenericForeignKey()
-
-    objects = AddressBookQuerySet.as_manager()
 
     class Meta:
-        indexes = [
-            models.Index(fields=["content_type", "object_id"]),
-        ]
         verbose_name = _("Phone Number")
         verbose_name_plural = _("Phone Numbers")
 
     def __str__(self):
         return self.number.as_national
+
+
+class Location(models.Model):
+    name = models.CharField(_("name"), max_length=100)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = _("Location")
+        verbose_name_plural = _("Locations")
+
+    def __str__(self):
+        return self.name
